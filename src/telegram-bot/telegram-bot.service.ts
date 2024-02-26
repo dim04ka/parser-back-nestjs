34dc37ab0b2173bcccc722d46b9cd65e390ac34f
nesta_dima <nesta_dima@mail.ru>
2024-02-26 20:19:49 +0400

    fix2

--- src/telegram-bot/telegram-bot.service.ts
+++ src/telegram-bot/telegram-bot.service.ts
@@ -31,7 +31,7 @@ export class TelegramBotService {
           caption: i === 0 ? caption : '',
         });
       }
-
+      console.log('media=', media);
       await this.bot.telegram.sendMediaGroup(chatId, media);
 
       // const imageData = await this.downloadImage(imageUrl);
